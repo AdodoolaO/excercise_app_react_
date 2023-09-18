fed02441b2f51b8e46e21c5c0caea2e3dcc73660
“adodoolaomiyale” <58596404+AdodoolaO@users.noreply.github.com>
2023-09-18 16:49:02 +0100

    Added styling

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -2,9 +2,26 @@ import React from 'react'
 import {Stack, Typography} from "@mui/material";
 import icon from "../assets/icons/gym.png"
 
-const BodyPart = () => {
+const BodyPart = (item, setBodyPart,bodyPart) => {
   return (
-    <Stack>
+    <Stack
+    type="button"
+    alignItems="center"
+    justifyContent="center"
+    className='bodyPart-card'
+    sx=
+{{
+borderTop:bodyPart === item ? '4px solid #FF2625' : "",
+  backgroundColor: '#fff',
+  borderBottomLeftRadius: "20px",
+  width:"270px",
+  height: "280px",
+  cursor:"pointer", gap:"47px"
+
+}}
+
+
+>
 <img src={icon} alt='dumbbell' className='body-part-icon' styles={{ width:"40px", 
 height:"40px"}}/>
     </Stack>
