91206817754862bc84d94da1a7109f8ec09d6f58
“adodoolaomiyale” <58596404+AdodoolaO@users.noreply.github.com>
2023-09-23 13:24:24 +0100

    Update BodyPart.js
    Added icon

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,29 +1,18 @@
 import React from 'react'
 import {Stack, Typography} from "@mui/material";
-import icon from "../assets/icons/gym.png"
+import Icon from "../assets/icons/gym.png"
 
-const BodyPart = (item, setBodyPart,bodyPart) => {
+const BodyPart = (item, setBodyPart, bodyPart) => {
   return (
     <Stack
-    type="button"
+    type ="button"
     alignItems="center"
     justifyContent="center"
     className='bodyPart-card'
-    sx=
-{{
-borderTop:bodyPart === item ? '4px solid #FF2625' : "",
-  backgroundColor: '#fff',
-  borderBottomLeftRadius: "20px",
-  width:"270px",
-  height: "280px",
-  cursor:"pointer", gap:"47px"
+    
+    >
+<img src={Icon} alt='dumbbell' style={{ width:"40px", height:"40px"}}/>
 
-}}
-
-
->
-<img src={icon} alt='dumbbell' className='body-part-icon' styles={{ width:"40px", 
-height:"40px"}}/>
     </Stack>
   )
 }
